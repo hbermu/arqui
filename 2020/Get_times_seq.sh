d9f221c08bfaa74c6ff632ff090688472a1eef2d
Hector <hbermudez@stratio.com>
2020-11-18 19:24:35 +0100

    Added script to test times

--- 2020/Get_times_seq.sh
+++ 2020/Get_times_seq.sh
@@ -1,22 +1,49 @@
-#!/bin/sh
+#!/bin/bash
 
 make all
 
 echo "### Seq Opt"
-# shellcheck disable=SC2039
+echo "#Copy"
 for i in {1..20}
 do
-    echo "Attempt $i"
+    echo "Attempt ${i}"
     time ./image-seq copy ./images_source ./images_destination
+    rm -rf images_destination/*.bmp
+done
+echo "#Gauss"
+for i in {1..20}
+do
+    echo "Attempt ${i}"
+    time ./image-seq gauss ./images_source ./images_destination
+    rm -rf images_destination/*.bmp
+done
+echo "#Sobel"
+for i in {1..20}
+do
+    echo "Attempt ${i}"
+    time ./image-seq sobel ./images_source ./images_destination
     rm -rf images_destination/*.bmp
 done
 
 echo "### Seq No Opt"
-# shellcheck disable=SC2039
 for i in {1..20}
 do
-    echo "Attempt $i"
+    echo "Attempt ${i}"
     time ./image-seq-nop copy ./images_source ./images_destination
+    rm -rf images_destination/*.bmp
+done
+echo "#Gauss"
+for i in {1..20}
+do
+    echo "Attempt ${i}"
+    time ./image-seq-nop gauss ./images_source ./images_destination
+    rm -rf images_destination/*.bmp
+done
+echo "#Sobel"
+for i in {1..20}
+do
+    echo "Attempt ${i}"
+    time ./image-seq-nop sobel ./images_source ./images_destination
     rm -rf images_destination/*.bmp
 done
 
